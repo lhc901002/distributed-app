77a1101814bcf173fb4bc63621e6e23986535030
lhc901002 <haocheng.liu@hotmail.com>
2016-07-15 22:58:55 +0800

    create nio server and client for message using netty

---
+++ nio/nio-client/src/main/java/org/mliuframework/nio/handler/MessageClientHandler.java
@@ -0,0 +1,53 @@
+package org.mliuframework.nio.handler;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * Created by Michael on 2016/7/15.
+ */
+public class MessageClientHandler extends ChannelInboundHandlerAdapter {
+
+    private final ByteBuf firstMessage;
+
+    /**
+     * Creates a client-side handler.
+     */
+    public MessageClientHandler(String message) {
+        byte[] req = message.getBytes();
+        firstMessage = Unpooled.buffer(req.length);
+        firstMessage.writeBytes(req);
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) {
+        ctx.writeAndFlush(firstMessage);
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        ByteBuf in = (ByteBuf) msg;
+        try {
+            while (in.isReadable()) { // (1)
+                System.out.print((char) in.readByte());
+            }
+        } catch(Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) {
+        ctx.flush();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        // Close the connection when an exception is raised.
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+}
---
+++ nio/nio-server/src/main/java/org/mliuframework/nio/handler/MessageServerHandler.java
@@ -0,0 +1,44 @@
+package org.mliuframework.nio.handler;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandler.Sharable;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * Created by Michael on 2016/7/15.
+ */
+@Sharable
+public class MessageServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        ByteBuf in = (ByteBuf) msg;
+        try {
+            while (in.isReadable()) { // (1)
+                System.out.print((char) in.readByte());
+                System.out.flush();
+            }
+        } catch(Exception e) {
+            System.out.println(e);
+        }
+        byte[] req = "Michael".getBytes();
+        ByteBuf firstMessage = Unpooled.buffer(req.length);
+        firstMessage.writeBytes(req);
+        ctx.writeAndFlush(firstMessage);
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) {
+        ctx.flush();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        // Close the connection when an exception is raised.
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+}
